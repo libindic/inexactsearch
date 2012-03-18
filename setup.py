5a646f8509364e8bc28707b5ca5826f12b0278e2
Vasudev Kamath <kamathvasudev@gmail.com>
2012-03-18 13:23:30 +0530

    InexactSearch module initialized

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup (
+    name = "inexactsearch",
+    version = "0.1",
+    url = "http://silpa.org.in/ApproxSearch",
+    license = "LGPL-3.0",
+    author = "Santhosh Thottingal",
+    author_email = "santhosh.thottingal@gmail.com",
+    description = "Fuzzy String Search for Indian languages",
+    long_description= "This library provides fuzzy string search\
+for Indian languages using Indic Soundex module.",
+    packages = find_packages('.'),
+    namespace_packages = ["inexactsearch"],
+    include_package_data = True,
+    setup_requires = ['setuptools-git'],
+    install_requires = ['setuptools','soundex'],
+    zip_safe = False,
+    )
