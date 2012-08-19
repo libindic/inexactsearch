5f88e1f7a0b47d0e399c33b0f99e6366662da3a6
Vasudev Kamath <kamathvasudev@gmail.com>
2012-08-19 12:43:50 +0530

    Resolving merge conflict

--- inexactsearch/__init__.py
+++ inexactsearch/__init__.py
@@ -1,26 +1,3 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD:__init__.py
-# Approximate Search
-# Copyright 2008 Santhosh Thottingal <santhosh.thottingal@gmail.com>
-# http://www.smc.org.in
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-__import__('pkg_resources').declare_namespace(__name__)
-from inexactsearch import InexactSearch, get_instance
-=======
 from inexactsearch.core import InexactSearch, getInstance
->>>>>>> testing:inexactsearch/__init__.py
 
