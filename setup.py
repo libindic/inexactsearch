b52755012f6924e3d78043007af47155a2a051b0
Vasudev Kamath <kamathvasudev@gmail.com>
2013-07-31 22:55:14 +0530

    Bumped version to 0.2, template now part of modules

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = "inexactsearch",
-    version = "0.2",
+    version = "0.3",
     url = "http://silpa.org.in/ApproxSearch",
     license = "LGPL-3.0",
     author = "Santhosh Thottingal",
