026217bf5287b0bdb45dfa25ebfd080f708f6998
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-25 18:33:42 +0530

    Rewrite setup.py to use pbr

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
 from setuptools import setup
 
-setup (
-    name = "inexactsearch",
-    version = "0.3",
-    url = "http://silpa.org.in/ApproxSearch",
-    license = "LGPL-3.0",
-    author = "Santhosh Thottingal",
-    author_email = "santhosh.thottingal@gmail.com",
-    description = "Fuzzy String Search for Indian languages",
-    long_description= "This library provides fuzzy string search\
-for Indian languages using Indic Soundex module.",
-    packages = ['inexactsearch'],
-    include_package_data = True,
-    setup_requires = ['setuptools-git'],
-    install_requires = ['setuptools','soundex'],
-    test_suite='tests',
-    zip_safe = False,
-    )
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
